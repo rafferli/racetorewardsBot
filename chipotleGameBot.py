e618b1771a3e3f7e70c5ddd6b8930bfb3782d904
rafferli <li.raffer0@gmail.com>
2021-06-26 08:56:49 -0500

    code cleanup and more comments

--- chipotleGameBot.py
+++ chipotleGameBot.py
@@ -1,3 +1,6 @@
+# Chipotle Race to Rewards Bot
+# https://racetorewards.com/
+
 from PIL import ImageGrab, ImageOps, Image
 from pyautogui import press
 import time
@@ -6,10 +9,8 @@ import numpy as np
 import keyboard
 
 # define blue color range
-
 lower_val_b = np.array([0,80,0]) 
 upper_val_b = np.array([107,255,255]) 
-
 
 # define green color range
 lower_val_g = np.array([45,173,198]) 
@@ -29,18 +30,19 @@ lower_val_s = np.array([101,21,0])
 upper_val_s = np.array([179,45,73])
 
 
-lane1Okay = True
+# we're gonna swap between lanes 2 and 3 cause it works :/
 lane2Okay = True
 lane3Okay = True
 
+#change to whatever lane the car will be in when the game starts
 currentLane = 2
 
 time.sleep(5)
 
-print("start")
+print("bot starting...")
 while True:
 
-        #pixel def: bbox=('LEFT SIDE X', 100, 'RIGHT SIDE X', 500)
+        #pixel def example: bbox=('LEFT SIDE X', 100, 'RIGHT SIDE X', 500)
 
         #for lane 2
         img2 =  np.array(ImageGrab.grab(all_screens=True, bbox=(877, 100, 1052, 500)))
@@ -71,27 +73,21 @@ while True:
 
 
         #LANE2
-        hasBlue = np.sum(mask_b2)
-        hasGreen = np.sum(mask_g2)
-        hasYellow = np.sum(mask_y2)
-        if (hasBlue + hasGreen + hasGreen) > 0:
+        if (np.sum(mask_b2) + np.sum(mask_g2) + np.sum(mask_y2)) > 0:
             lane2Okay = False
-            print("LANE 2 NOT OK")
+            print("Object in Lane 2")
         else:
             lane2Okay = True
 
         #LANE3
-        hasBlue = np.sum(mask_b3)
-        hasGreen = np.sum(mask_g3)
-        hasYellow = np.sum(mask_y3)
-        if (hasBlue + hasGreen + hasGreen) > 0:
+        if (np.sum(mask_b3) + np.sum(mask_g3) + np.sum(mask_y3)) > 0:
             lane3Okay = False
-            print("LANE 3 NOT OK")
+            print("Object in Lane 2")
         else:
             lane3Okay = True
 
 
-
+        # checks to see if the object is a bonus item. collets item if it is
         l2good = np.sum(mask_guac2) + np.sum(mask_s2)
         if l2good:
                 lane2Okay = True
@@ -101,7 +97,7 @@ while True:
                 lane3Okay = True
                 time.sleep(.1)
 
-
+        #move lane if necessary
         if currentLane == 2:
             if lane2Okay == False:
                 press("right")
